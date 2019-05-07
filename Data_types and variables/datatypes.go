7ce8679ae8603953035e99fdd38fe76214ee078d
Kevin Lin <kevinlin47@hotmail.com>
2019-05-06 22:12:34 -0400

    Remove main function from datatypes.go, create variable casting examples in variable_Casting.go

--- Data_types and variables/datatypes.go
+++ Data_types and variables/datatypes.go
@@ -1,13 +1,11 @@
 package main
 
-func main() {
-	//uint8 - Unsigned 8-bit integers (0 to 255)
-	//int16 - Signed 16-bit integers (-32768 to 32767)
-	//int32 - Signed 32-bit integers (-2147483648 to 2147483647)
-	//int64 - Signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+//uint8 - Unsigned 8-bit integers (0 to 255)
+//int16 - Signed 16-bit integers (-32768 to 32767)
+//int32 - Signed 32-bit integers (-2147483648 to 2147483647)
+//int64 - Signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
 
-	//float32 - IEEE-754 32-bit floating-point numbers
-	//float64 - IEEE 64-bit floating-point numbers
+//float32 - IEEE-754 32-bit floating-point numbers
+//float64 - IEEE 64-bit floating-point numbers
 
-	//rune - same as int32 (Unicode)
-}
+//rune - same as int32 (Unicode)
---
+++ Data_types and variables/variable_casting.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var numberOne = 3            //integer variable
+	var numberTwo float32 = 3.19 //float variable
+
+	// Can't add numberOne with numberTwo because of a mismatch in variable type
+	// We can cast numberOne to a float
+
+	numberOneAsFloat := float32(numberOne)
+
+	fmt.Println(numberOneAsFloat + numberTwo)
+
+	//convert float32 to float64
+	var myFloat32 float32 = 19.95
+	fmt.Println(myFloat32)
+
+	myFloat64 := float64(myFloat32)
+
+	fmt.Println(myFloat64)
+
+	//How to change a number represented as a string to an integer and vice versa?
+	myInt := 7
+	myString := "5"
+
+	//Can't add these because they are not the same type
+	stringAsInt, err := strconv.Atoi(myString)
+
+	if err == nil {
+		fmt.Println(myInt + stringAsInt)
+	}
+
+	//Convert integer to string
+	intAsString := strconv.Itoa(myInt)
+
+	fmt.Println(intAsString + myString) //Should print concatenation of "7" and "5", so string "75"
+}
