e38f2e82ce2028c7c44ad98143500b49d52d0636
Kevin Lin <kevinlin47@hotmail.com>
2019-05-29 18:49:10 -0400

    Final source code file for slices video

--- slices/slices.go
+++ slices/slices.go
@@ -40,4 +40,36 @@ func main() {
 
 	fmt.Println(mySlice)
 
+	/********* Examples using the slice operator *********/
+	newSlice := make([]int, 20)
+
+	for i := 0; i < len(newSlice); i++ {
+		newSlice[i] = i * 2
+	}
+
+	fmt.Println(newSlice)
+
+	newSlice2 := newSlice[0:10] //Create a new slice with elements from newSlice up to index 9
+
+	fmt.Println(newSlice2)
+
+	newSlice3 := newSlice[15:] //Create new slice with elements from end of newSlice up to index 15
+
+	fmt.Println(newSlice3)
+
+	//Example of removing ane element from a slice at a given index
+	//We will remove 18 from the slice newSlice
+	//The value of 18 is at index 9
+	newSlice = append(newSlice[:9], newSlice[10:]...)
+
+	//Explanation for the algorithm above to remove an element at a certain index
+	//The append function takes a source slice and adds the given elements to a new slice that is returned
+	//So as the source slice we use a slice containing all the elements up to the index of the element that we want to remove
+	//Using the slice operator, remember the value on the right side of the ":" is inclusive.
+	//Then we add onto that slice the remaning elements after the idnex of the element that we wanted is removed.
+	//So using the slice operator we want elements indexOfElementRemoved + 1 to the end.
+	//The ... is used for variadic parameters, in simple terms it will unpack all the elements from that slice.
+	//It is shorthand for writing append(newSlice[:9], newSlice[10], newSlice[11], newSlice[12], and so on)
+
+	fmt.Println(newSlice)
 }
