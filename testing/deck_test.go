eb3a1219345c789fb52674138c17e8d69199faee
Kevin Lin <kevinlin47@hotmail.com>
2019-07-02 19:36:05 -0400

    Update deck_test.go source code

--- testing/deck_test.go
+++ testing/deck_test.go
@@ -1,1 +1,22 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	//Make sure deck has 52 cards
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %d", len(d))
+	}
+
+	//Make sure first card is Ace of HEarts
+	if d[0] != "Ace of Hearts" {
+		t.Errorf("Expected first card of Ace of Hearts, but got %v", d[0])
+	}
+
+	//Make sure last is King of Diamonds
+	if d[len(d)-1] != "King of Diamonds" {
+		t.Errorf("Expected last card of King of Diamonds, but got %v", d[len(d)-1])
+	}
+}
