b9fd1b0461b1b204ef0a6908f5cad0fec71398fa
Kevin Lin <kevinlin47@hotmail.com>
2019-07-20 09:34:31 -0400

    Update source code file for structs.go and update syllabus

---
+++ structs/structs.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	age       int32
+}
+
+func main() {
+	//This syntax creates a new struct where each field must be in order
+	//as defined in the person struct, ie first field is firstName, second field is lastName
+	//, and third field is age
+	person1 := person{"Bruce", "Wayne", 30}
+
+	//You can name the fields when assigning values to them, here you can order it anyway
+	person2 := person{firstName: "Kevin", lastName: "Lin", age: 24}
+
+	//Omitting any fields results in that variable types zero-value
+	person3 := person{}
+
+	fmt.Println(person1)
+	fmt.Println(person2)
+	fmt.Println(person3)
+
+	//Using dot operator to access specific fields in the struct
+	fmt.Println("person1's first name: " + person1.firstName)
+	fmt.Println("person2's last name: " + person2.lastName)
+	fmt.Println("person3's age: ", person3.age)
+}
