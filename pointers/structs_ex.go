0b199d7d3ad5db343968e4a2032d64129cace83b
Kevin Lin <kevinlin47@hotmail.com>
2019-07-31 16:57:50 -0400

    Create Rame slide and source code file structs_ex.go

---
+++ pointers/structs_ex.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	kevin := person{
+		firstName: "Kevin",
+		lastName:  "Lin",
+		contactInfo: contactInfo{
+			email:   "example@example.com",
+			zipCode: 1234,
+		},
+	}
+	kevin.updateEmail("myNewEmail@example.com")
+	kevin.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateEmail(newEmail string) {
+	p.contactInfo.email = newEmail
+}
