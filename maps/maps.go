411fbe9b6da75b7b340706db81f930b64cb9868d
Kevin Lin <kevinlin47@hotmail.com>
2019-08-09 09:52:58 -0400

    Complete declaring map slide and create adding to a map slide

---
+++ maps/maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+	}*/
+
+	/*var colors map[string]string*/
+
+	colors := make(map[string]string)
+
+	fmt.Println(colors)
+}
