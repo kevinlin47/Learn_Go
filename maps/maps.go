b8562cd24b42e76710a4605a3a31fdd658852152
Kevin Lin <kevinlin47@hotmail.com>
2019-08-09 10:02:47 -0400

    Create iterating over map slide

--- maps/maps.go
+++ maps/maps.go
@@ -1,19 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	/*colors := map[string]string{
+	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
-	}*/
-
-	/*var colors map[string]string*/
-
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+		"white": "#ffffff",
+	}
 }
