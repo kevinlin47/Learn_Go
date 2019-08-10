2b66be510ebf5869b4423b5da76d5b5762a026db
Kevin Lin <kevinlin47@hotmail.com>
2019-08-10 13:26:02 -0400

    maps.go source file

---
+++ maps/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println("[", key, "]", " -> ", value)
+	}
+}
